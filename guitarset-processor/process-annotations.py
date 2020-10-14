53dc88022398597d35caac4c4e3e4cbd801ebf53
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2020-10-14 16:34:37 +0200

    Cleaned-up

--- guitarset-processor/process-annotations.py
+++ guitarset-processor/process-annotations.py
@@ -10,7 +10,7 @@ SEGMENT_LENGTH = 0.2 # 0.2 seconds
 def get_annotation_files(n):
     files = filter(lambda file: file[-5:] == ".jams", os.listdir(ANNOTATIONS_ROOT))
     files = list(files)[0:n]
-    return list(map(lambda file: jams.load(ANNOTATIONS_ROOT + file), files))
+    return list(map(lambda file: jams.load(os.path.join(ANNOTATIONS_ROOT, file)), files))
 
 def compute_overlap_frequency(jam, segment_length):
     strings = jam.search(namespace="note_midi")
@@ -82,11 +82,7 @@ def get_segmented_input(jam):
 def to_guitar_chords(midi_values):
     # Initialize variables
     frets = np.zeros((6, 18), dtype=np.int32)
-    #output = np.copy(frets)
     
-    #f_row = np.full((6, 6), np.inf)  # 6 strings with 1 note per string
-    #f_col = np.full((6, 6), np.inf)
-
     # Retrieve all possible notes played
     for q in range(0, 6):
         qs = [40, 45, 50, 55, 59, 64]
@@ -102,71 +98,77 @@ def to_guitar_chords(midi_values):
 
     return tab
 
-    """
-    fcnt2 = 0
+"""
+output = np.copy(frets)
+f_row = np.full((6, 6), np.inf)  # 6 strings with 1 note per string
+f_col = np.full((6, 6), np.inf)
 
-    for t in range(0, len(midi_values)):
-        fret_played = (frets == int(midi_values[t])) * 1
+<code>
 
-        cng = 0
-        for dr in range(0, len(frets[:, 0])):
-            for dc in range(0, len(frets[0, :])):
-                if fret_played[dr, dc] == 1:
-                    if cng == 0:
-                        fcnt2 = 0
-                        cng += 1
-                    f_row[t, fcnt2] = dr
-                    f_col[t, fcnt2] = dc
-                    fcnt2 += 1
-    
-        print(fret_played)
-    print(f_row)
-    print(f_col)
+fcnt2 = 0
 
-    # Initialize the 6 possible note solutions (one note per string)
-    f_sols = [np.copy(f_col) for _ in range(6)]
+for t in range(0, len(midi_values)):
+    fret_played = (frets == int(midi_values[t])) * 1
 
-    pri_cnt_c, = np.where(np.isfinite(f_col[0, :]))
-    pri_cnt_r, = np.where(np.isfinite(f_col[:, 0]))
-    print(pri_cnt_c)
-    print(pri_cnt_r)
-    for pri in range(0, len(pri_cnt_c)):
-        for sub_r in range(1, 6):
-            for sub_c in range(0, len(f_sols[0][0, :])):
-                f_sols[pri][sub_r, sub_c] = abs(f_col[0, pri] - f_col[sub_r, sub_c])
-    
-    if len(pri_cnt_r) == 0 or len(pri_cnt_c) == 0:
-        true_tab = np.copy(np.zeros((6, 18), dtype=np.int32))
-        print("tab", true_tab)
-    else:
-        ck_sols = [np.zeros((len(pri_cnt_r) - 1, len(pri_cnt_c) - 1), dtype = np.int32) for _ in range(6)]
-        sol_inds = [np.copy(ck_sols[0]) for _ in range(6)]
+    cng = 0
+    for dr in range(0, len(frets[:, 0])):
+        for dc in range(0, len(frets[0, :])):
+            if fret_played[dr, dc] == 1:
+                if cng == 0:
+                    fcnt2 = 0
+                    cng += 1
+                f_row[t, fcnt2] = dr
+                f_col[t, fcnt2] = dc
+                fcnt2 += 1
 
-        # Replace infinite values with high finite values for each solution
-        for ck_sol in range(6):
-            for pri_sol_r in range(1, len(pri_cnt_r)):
-                for pri_sol_c in range(0, len(pri_cnt_c) - 1):  # Random - 1
-                    infinites = np.isinf(f_sols[ck_sol][pri_sol_r, :]) 
-                    if np.any(infinites):
-                        f_sols[ck_sol][pri_sol_r, np.argwhere(infinites)] = 999
+    print(fret_played)
+print(f_row)
+print(f_col)
 
-                    if ck_sol > 0:
-                        ck_sols[ck_sol][0, pri_sol_c] = min(f_sols[ck_sol][pri_sol_r, :])
+# Initialize the 6 possible note solutions (one note per string)
+f_sols = [np.copy(f_col) for _ in range(6)]
 
-        # Determine "rating" for each solution
-        tab_sols = [np.argmin(f_sols[i], axis = 1) for i in range(6)]
-        min_sols = [np.min(f_sols[i], axis = 1) for i in range(6)]
+pri_cnt_c, = np.where(np.isfinite(f_col[0, :]))
+pri_cnt_r, = np.where(np.isfinite(f_col[:, 0]))
+print(pri_cnt_c)
+print(pri_cnt_r)
+for pri in range(0, len(pri_cnt_c)):
+    for sub_r in range(1, 6):
+        for sub_c in range(0, len(f_sols[0][0, :])):
+            f_sols[pri][sub_r, sub_c] = abs(f_col[0, pri] - f_col[sub_r, sub_c])
 
-        for i in range(6):
-            inf = np.isinf(min_sols[i][:])
-            if np.any(inf):
-                min_sols[np.argwhere(inf)] = 0
+if len(pri_cnt_r) == 0 or len(pri_cnt_c) == 0:
+    true_tab = np.copy(np.zeros((6, 18), dtype=np.int32))
+    print("tab", true_tab)
+else:
+    ck_sols = [np.zeros((len(pri_cnt_r) - 1, len(pri_cnt_c) - 1), dtype = np.int32) for _ in range(6)]
+    sol_inds = [np.copy(ck_sols[0]) for _ in range(6)]
 
-        sols = [np.sum(min_sols[i][:]) for i in range(6)]
+    # Replace infinite values with high finite values for each solution
+    for ck_sol in range(6):
+        for pri_sol_r in range(1, len(pri_cnt_r)):
+            for pri_sol_c in range(0, len(pri_cnt_c) - 1):  # Random - 1
+                infinites = np.isinf(f_sols[ck_sol][pri_sol_r, :]) 
+                if np.any(infinites):
+                    f_sols[ck_sol][pri_sol_r, np.argwhere(infinites)] = 999
 
-        print("sols:")
-        print(sols)
-    """
+                if ck_sol > 0:
+                    ck_sols[ck_sol][0, pri_sol_c] = min(f_sols[ck_sol][pri_sol_r, :])
+
+    # Determine "rating" for each solution
+    tab_sols = [np.argmin(f_sols[i], axis = 1) for i in range(6)]
+    min_sols = [np.min(f_sols[i], axis = 1) for i in range(6)]
+
+    for i in range(6):
+        inf = np.isinf(min_sols[i][:])
+        if np.any(inf):
+            min_sols[np.argwhere(inf)] = 0
+
+    sols = [np.sum(min_sols[i][:]) for i in range(6)]
+
+    print("sols:")
+    print(sols)
+"""
 
 def visualize_overlap_frequencies(parsed_jams):
     import matplotlib.pyplot as plt
