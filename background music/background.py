52dc7ab8e9dbb0aa2acf15496047f2f7b47e042d
paulbarake <pobarak@hotmail.fr>
2020-10-22 16:27:27 +0200

    background 2

---
+++ background music/background.py
@@ -0,0 +1,18 @@
+import matplotlib
+import librosa
+import numpy as np
+import librosa.display
+import librosa.output
+
+
+example_audio = librosa.util.example_audio_file()
+x, sr = librosa.load(example_audio, duration=5)
+print('shape of x ==> ' + str(x.shape))
+y, sr = librosa.load(example_audio, duration=5)
+print('shape of y ==> ' + str(y.shape))
+z = np.append(x,y)
+print('shape of x+y = z ==> ' + str(z.shape))
+librosa.output.write_wav('joined_file.wav', z, sr)
+
+z_loaded, sr = librosa.load('joined_file.wav')
+print('shape of z loaded ==> ' + str(z_loaded.shape))
