c91486fc90a3b31525460489420db89541096c4c
Pierrick Chebel <pchebel@gmail.com>
2020-10-28 16:45:18 +0100

    Added a server to test the upload function

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var multer = require('multer')
+var cors = require('cors');
+
+var app = express();
+
+const PORT = 8000;
+
+app.use(cors())
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+})
+
+var upload = multer({ storage: storage }).single('file')
+
+app.post('/upload', function (req, res) {
+    upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            return res.status(500).json(err)
+        } else if (err) {
+            return res.status(500).json(err)
+        }
+        return res.status(200).send(req.file)
+    })
+});
+
+app.listen(PORT, function () {
+    console.log('App runnning on port ' + PORT);
+});
