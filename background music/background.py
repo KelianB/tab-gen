21043f4c03f86fbc862c70fd07d79a675d5cb3d4
paulbarake <pobarak@hotmail.fr>
2020-11-04 22:01:45 +0100

    Soundfile

--- background music/background.py
+++ background music/background.py
@@ -1,8 +1,7 @@
 import matplotlib
 import librosa
 import numpy as np
-import librosa.display
-import librosa.output
+import soundfile as sf
 
 
 example_audio = librosa.util.example_audio_file()
@@ -12,7 +11,7 @@ y, sr = librosa.load(example_audio, duration=5)
 print('shape of y ==> ' + str(y.shape))
 z = np.append(x,y)
 print('shape of x+y = z ==> ' + str(z.shape))
-librosa.output.write_wav('joined_file.wav', z, sr)
+sf.write('joined_file.wav', z, sr)
 
 z_loaded, sr = librosa.load('joined_file.wav')
 print('shape of z loaded ==> ' + str(z_loaded.shape))
