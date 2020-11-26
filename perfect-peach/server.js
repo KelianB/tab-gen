aa8d1b188d97ea39737871b7afc8eb18e177d331
Pierrick Chebel <pchebel@gmail.com>
2020-11-27 00:17:39 +0100

    Début du Mock du back
    Il faut encore ajouter/déclencher un timer pour simuler le temps de calcul du modèle

--- perfect-peach/server.js
+++ perfect-peach/server.js
@@ -4,6 +4,9 @@ var cors = require('cors');
 
 var app = express();
 
+
+const versions = require("./mock_back/versions.json");
+const { version } = require('react');
 const PORT = 8000;
 
 app.use(cors())
@@ -31,7 +34,49 @@ app.post('/upload', function (req, res) {
         return res.status(200).sendFile(__dirname + '/public/static/files/example.atex')
         //return res.status(200).send(req.file)
     })
+})
+
+.get('/api/versions', (req,res) => {
+
+    res.status(200).json(versions);
+})
+
+.get('/api/version/:verion_id/:job_id/state', (req,res) => {
+
+    // ADD A TIMER TO MOCK THE TAB PROCESSING TIME
+
+    res.state(200).json({etat:false})
+})
+
+.get('/api/version/:version_id/:job_id/result', (req,res) => {
+
+    // ADD A TIMER TO MOCK THE TAB PROCESSING TIME
+
+    res.status(200).sendFile(__dirname + '/public/static/files/example.atex');
+})
+
+
+.post('/api/version/:version_id', (req,res) => {
+
+    upload(req, res, (err) => {
+        if (err instanceof multer.MulterError) {
+            return res.status(500).json(err)
+        } else if (err) {
+            return res.status(500).json(err)
+        }
+
+        //ADD A TIMER TO MOCK THE TAB PROCESSING TIME
+        
+        const mock_job_id = 1;
+
+        return res.status(200).json({job_id:mock_job_id});
+    })
+
+
+
 });
+
+
 
 app.listen(PORT, function () {
     console.log('App runnning on port ' + PORT);
