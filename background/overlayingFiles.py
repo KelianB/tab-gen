a45ac56269cc36e175ed59090c3682b3ce4e4bb6
paulbarake <pobarak@hotmail.fr>
2020-11-26 19:15:42 +0100

    MassOverlay

--- background/overlayingFiles.py
+++ background/overlayingFiles.py
@@ -1,10 +1,24 @@
 from pydub import AudioSegment
+import glob 
 
-sound1 = AudioSegment.from_mp3("background/summertime.mp3")
-sound2 = AudioSegment.from_mp3("background/takefive.mp3")
-
+"""
 # mix sound2 with sound1, starting at 5000ms into sound1)
 output = sound1.overlay(sound2, position=5000)
-
 # save the result
-output.export("mixed_sounds.mp3", format="mp3")
+output.export("mixed_sounds.mp3", format="mp3") """
+
+
+def overlaying(sound1, sound2, position, output_name) :
+    output = sound1.overlay(sound2, position=position)
+    output.export(output_name, format="mp3")
+
+
+
+
+
+# TEST ZONE
+sound1 = AudioSegment.from_mp3("background/summertime.mp3")
+
+sound2 = AudioSegment.from_mp3("background/takefive.mp3")
+
+overlaying(sound1, sound2, 500, "test.mp3")
---
+++ background/overlayingGuitarSet.py
@@ -0,0 +1,65 @@
+from pydub import AudioSegment
+import glob 
+
+# fonction d'overlay d'une piste avec une seconde
+def overlaying(sound1, sound2, position, output_name) :
+    output = sound1.overlay(sound2, position = position)
+    output.export(output_name, format="mp3")
+
+
+#path des morceaux du dataset (à modifier)
+path1 = "../../Music/Musique/Mp3_files/Caravan/*.*"
+path2 = "overlayedDrums/*.*"
+
+# importer les différents backgrounds
+background_drum1 = AudioSegment.from_mp3("background/drum_background1.mp3")
+background_drum2 = AudioSegment.from_mp3("background/drum_background2.mp3")
+
+background_bass1 = AudioSegment.from_mp3("background/bass_background1.mp3")
+background_bass2 = AudioSegment.from_mp3("background/bass_background2.mp3")
+
+
+# fonctions d'overlay de l'ensemble des morceaux du path
+def massOverlayDrum(path) :
+    count = 0
+    # overlay de chaque fichier avec un des backgrounds en alternance
+    for file in glob.glob(path) :
+        original_sound = AudioSegment.from_mp3(file)
+        if (count % 2 == 0 ) :
+            background = background_drum1
+            backing_name = "1"
+        else :
+            background = background_drum2
+            backing_name = "2"
+        # nommer le fichier fils
+        raw_file_name = file.split("\\",1)[1]
+        overlayed_file_name = "overlayedDrums/%s_overlay%s.mp3" %(raw_file_name.split(".mp3",1)[0], backing_name)
+        # créer le fichier fils
+        overlaying(original_sound, background, 0, overlayed_file_name)
+        # print(overlayed_file_name)
+        count += 1
+
+def massOverlayBass(path) :
+    count = 0
+    # overlay de chaque fichier avec un des backgrounds en alternance
+    for file in glob.glob(path) :
+        original_sound = AudioSegment.from_mp3(file)
+        if (count % 2 == 0 ) :
+            background = background_bass1
+            backing_name = "1"
+        else :
+            background = background_bass2
+            backing_name = "2"
+        # nommer le fichier fils
+        raw_file_name = file.split("\\",1)[1]
+        overlayed_file_name = "overlayedDrumsAndBass/%s_overlay%s.mp3" %(raw_file_name.split(".mp3",1)[0], backing_name)
+        # créer le fichier fils
+        overlaying(original_sound, background, 0, overlayed_file_name)
+        # print(overlayed_file_name)
+        count += 1
+
+
+
+# test
+# massOverlayDrum(path1)
+# massOverlayBass(path2)
