95b400f0df0ff3e8792319a1d3580e92d8f68cf9
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2021-01-04 12:32:38 +0100

    added some models

--- guitarset/model/saved_models.py
+++ guitarset/model/saved_models.py
@@ -324,3 +324,336 @@ class GuitarSetModel(BaseModel):
         return torch.stack(outs, dim=1)
 
 """
+
+""" #17
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from model.resnet import ResNetBasicBlock
+from base import BaseModel      
+
+class GuitarSetModel(BaseModel):
+    def __init__(self):
+        super().__init__()
+
+        self.main_block = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=3, padding=1),          
+            nn.ReLU(),
+
+            ResNetBasicBlock(8, 16),
+                     
+            nn.Dropout(0.2),
+            nn.BatchNorm2d(16), # added for gradient stability
+            nn.Conv2d(16, 16, kernel_size=3, padding=1),         
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
+
+        self.out_layers = nn.ModuleList([
+            nn.Sequential(
+                ResNetBasicBlock(16, 8),
+                nn.Flatten(),
+                nn.Dropout(0.2),
+                nn.Linear(8*60*80, 128),
+                nn.ReLU(),
+                nn.Dropout(0.2),
+                nn.BatchNorm1d(128),
+                nn.Linear(128, 19),
+                nn.Softmax(dim=1),
+            ) for i in range(6)
+        ])
+
+    def forward(self, x): 
+        x = self.main_block(x)
+        outs = [sequential(x) for sequential in self.out_layers]
+        return torch.stack(outs, dim=1)
+"""
+
+""" #30
+
+class GuitarSetModel(BaseModel):
+    def __init__(self):
+        super().__init__()
+    
+        self.main_block = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=3, padding=1),          
+            nn.ReLU(),
+
+            nn.BatchNorm2d(8), # added for gradient stability
+            nn.Conv2d(8, 16, kernel_size=3, padding=1),          
+            nn.ReLU(),
+                     
+            #nn.Dropout(0.2),
+            #nn.BatchNorm2d(16), # added for gradient stability
+            nn.Conv2d(16, 8, kernel_size=3, padding=1),         
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Flatten(),        
+        )
+
+        self.out_layers = nn.ModuleList([
+            nn.Sequential(
+                nn.Dropout(0.2),
+                
+                nn.Linear(8*60*80, 256),
+                nn.ReLU(),
+                #nn.Dropout(0.2),
+                #nn.BatchNorm1d(128),
+ 
+                nn.Linear(256, 19),
+                nn.Softmax(dim=1),
+            ) for i in range(6)
+        ])
+
+    def forward(self, x): 
+        x = self.main_block(x)
+        outs = [sequential(x) for sequential in self.out_layers]
+        return torch.stack(outs, dim=1)
+"""
+
+"""
+33
+class GuitarSetModel(BaseModel):
+    def __init__(self):
+        super().__init__()
+    
+        self.main_block = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=3, padding=1),          
+            nn.ReLU(),
+
+            nn.BatchNorm2d(8), # added for gradient stability
+            nn.Conv2d(8, 16, kernel_size=3, padding=1),          
+            nn.ReLU(),
+                     
+            #nn.Dropout(0.2),
+            #nn.BatchNorm2d(16), # added for gradient stability
+            nn.Conv2d(16, 8, kernel_size=3, padding=1),         
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Flatten(), 
+            nn.Dropout(0.2),
+            nn.Linear(8*60*80, 512), 
+            nn.ReLU(),      
+        )
+
+        self.out_layers = nn.ModuleList([
+            nn.Sequential(
+                nn.Linear(512, 256),
+                nn.ReLU(),
+
+                nn.Dropout(0.2),
+                nn.Linear(256, 256),
+                nn.ReLU(),
+                
+                nn.Linear(256, 128),
+                nn.ReLU(),
+                #nn.Dropout(0.2),
+                #nn.BatchNorm1d(128),
+ 
+                nn.Linear(128, 19),
+                nn.Softmax(dim=1),
+            ) for i in range(6)
+        ])
+
+    def forward(self, x): 
+        x = self.main_block(x)
+        outs = [sequential(x) for sequential in self.out_layers]
+        return torch.stack(outs, dim=1)
+"""
+
+"""
+34
+class GuitarSetModel(BaseModel):
+    def __init__(self):
+        super().__init__()
+    
+        self.main_block = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=3, padding=1),          
+            nn.ReLU(),
+
+            nn.BatchNorm2d(8), # added for gradient stability
+            nn.Conv2d(8, 16, kernel_size=3, padding=1),          
+            nn.ReLU(),
+                     
+            #nn.Dropout(0.2),
+            #nn.BatchNorm2d(16), # added for gradient stability
+            nn.Conv2d(16, 8, kernel_size=3, padding=1),         
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Flatten(), 
+            nn.Dropout(0.2),
+            nn.Linear(8*60*80, 256), 
+            nn.ReLU(),      
+        )
+
+        self.out_layers = nn.ModuleList([
+            nn.Sequential(
+                nn.Dropout(0.2),
+                nn.Linear(256, 256),
+                nn.ReLU(),
+                
+                nn.Linear(256, 128),
+                nn.ReLU(),
+                #nn.Dropout(0.2),
+                #nn.BatchNorm1d(128),
+ 
+                nn.Linear(128, 19),
+                nn.Softmax(dim=1),
+            ) for i in range(6)
+        ])
+
+    def forward(self, x): 
+        x = self.main_block(x)
+        outs = [sequential(x) for sequential in self.out_layers]
+        return torch.stack(outs, dim=1)
+
+"""
+
+"""
+35
+class GuitarSetModel(BaseModel):
+    def __init__(self):
+        super().__init__()
+    
+        self.main_block = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=3, padding=1),          
+            nn.ReLU(),
+
+            #nn.BatchNorm2d(8), # added for gradient stability
+            nn.Conv2d(8, 16, kernel_size=3, padding=1),          
+            nn.ReLU(),
+                     
+            #nn.Dropout(0.2),
+            #nn.BatchNorm2d(16), # added for gradient stability
+            nn.Conv2d(16, 8, kernel_size=3, padding=1),         
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Flatten(), 
+            #nn.Dropout(0.2),
+            nn.Linear(8*60*80, 256), 
+            nn.ReLU(),      
+        )
+
+        self.out_layers = nn.ModuleList([
+            nn.Sequential(
+                #nn.Dropout(0.2),
+                nn.Linear(256, 256),
+                nn.ReLU(),
+                
+                nn.Linear(256, 128),
+                nn.ReLU(),
+                #nn.Dropout(0.2),
+                #nn.BatchNorm1d(128),
+ 
+                nn.Linear(128, 19),
+                nn.Softmax(dim=1),
+            ) for i in range(6)
+        ])
+
+    def forward(self, x): 
+        x = self.main_block(x)
+        outs = [sequential(x) for sequential in self.out_layers]
+        return torch.stack(outs, dim=1)
+"""
+
+"""
+37
+class GuitarSetModel(BaseModel):
+    def __init__(self):
+        super().__init__()
+    
+        self.main_block = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=9, padding=4),          
+            nn.ReLU(),
+
+            nn.BatchNorm2d(8), # added for gradient stability
+            nn.Conv2d(8, 16, kernel_size=7, padding=3),          
+            nn.ReLU(),
+                     
+            #nn.Dropout(0.2),
+            #nn.BatchNorm2d(16), # added for gradient stability
+            nn.Conv2d(16, 8, kernel_size=5, padding=2),         
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Flatten(), 
+            nn.Dropout(0.2),
+            nn.Linear(8*60*80, 256), 
+            nn.ReLU(),      
+        )
+
+        self.out_layers = nn.ModuleList([
+            nn.Sequential(
+                nn.Dropout(0.2),
+                nn.Linear(256, 256),
+                nn.ReLU(),
+                
+                nn.Linear(256, 128),
+                nn.ReLU(),
+                #nn.Dropout(0.2),
+                #nn.BatchNorm1d(128),
+ 
+                nn.Linear(128, 19),
+                nn.Softmax(dim=1),
+            ) for i in range(6)
+        ])
+
+    def forward(self, x): 
+        x = self.main_block(x)
+        outs = [sequential(x) for sequential in self.out_layers]
+        return torch.stack(outs, dim=1)
+
+"""
+
+"""
+38
+
+class GuitarSetModel(BaseModel):
+    def __init__(self):
+        super().__init__()
+    
+        self.main_block = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=3, padding=1),          
+            nn.ReLU(),
+
+            nn.BatchNorm2d(8), # added for gradient stability
+            nn.Conv2d(8, 16, kernel_size=3, padding=1),          
+            nn.ReLU(),
+                     
+            nn.Conv2d(16, 8, kernel_size=3, padding=1),         
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(8, 4, kernel_size=3, padding=1),         
+            nn.ReLU(),
+            
+            nn.Flatten(), 
+            nn.Dropout(0.2),
+            nn.Linear(4*60*80, 512), 
+            nn.ReLU(),      
+        )
+
+        self.out_layers = nn.ModuleList([
+            nn.Sequential(
+                nn.Dropout(0.2),
+                nn.Linear(512, 256),
+                nn.ReLU(),
+                
+                nn.Linear(256, 128),
+                nn.ReLU(),
+ 
+                nn.Linear(128, 19),
+                nn.Softmax(dim=1),
+            ) for i in range(6)
+        ])
+
+    def forward(self, x): 
+        x = self.main_block(x)
+        outs = [sequential(x) for sequential in self.out_layers]
+        return torch.stack(outs, dim=1)
+
+"""
