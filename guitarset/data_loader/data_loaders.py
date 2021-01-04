da8bbcf4862cc9aa8a2289c37075cf08789976a9
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2021-01-04 12:33:58 +0100

    commented out unused data loader

--- guitarset/data_loader/data_loaders.py
+++ guitarset/data_loader/data_loaders.py
@@ -2,10 +2,10 @@ from torchvision import datasets, transforms
 from base import BaseDataLoader
 from data_loader.guitarset_dataset import GuitarSetDataset
 
+"""
+# Example Data Loader
 class MnistDataLoader(BaseDataLoader):
-    """
-    MNIST data loading demo using BaseDataLoader
-    """
+    #MNIST data loading demo using BaseDataLoader
     def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
         trsfm = transforms.Compose([
             transforms.ToTensor(),
@@ -14,6 +14,7 @@ class MnistDataLoader(BaseDataLoader):
         self.data_dir = data_dir
         self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+"""
 
 class GuitarSetDataLoader(BaseDataLoader):
     """
