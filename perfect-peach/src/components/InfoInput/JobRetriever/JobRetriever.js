c63ad24cd28e21c99bccf27c84309465ba9fcd03
Pierrick Chebel <pchebel@gmail.com>
2021-02-27 00:11:50 +0100

    JobRetriever: Same pb

--- perfect-peach/src/components/InfoInput/JobRetriever/JobRetriever.js
+++ perfect-peach/src/components/InfoInput/JobRetriever/JobRetriever.js
@@ -40,8 +40,8 @@ class JobRetriever extends React.Component {
                     <div className = "separation"/>
                     <label className= "upload-title"> PREVIOUS SCORES </label>
 
-                    {jobs.map((job_id => {
-                        return <button type="button" class="btn upload-button btn-block" onClick={() => this.onClickHandler(job_id)}> Show Job {job_id} Status </button>
+                    {jobs.map(( (job_id,index) => {
+                        return <button key={job_id + index} type="button" class="btn upload-button btn-block" onClick={() => this.onClickHandler(job_id)}> Show Job {job_id} Status </button>
                     }))}
                 </div>)
         } else {
